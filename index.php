1acd21f5da2d665e7fa0e5877d0ef8beaf11d8f8
aisyahjamal <nuraisyahjamal99@gmail.com>
2021-11-12 21:08:35 +0800

    Loop & UI added

--- index.php
+++ index.php
@@ -2,16 +2,37 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap">
+        <title>ISS LOCATION FINDER</title>
     </head>
     <body>
-        <h1>Lets Go </h1>
 
+    <style>
+    body {
+        background: #152238 }
+    section {
+        background: black;
+        color: white;
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+    </style>
+    <section>
+        <h1> Enter your choice of date & time </h1>
         <form action="location.php">
             <input type="datetime-local" placeholder="Search.." name="search">
             <button type="submit">Submit</button>
         </form>
+    </section>
 
     </body>
+
+   
 
 </html>
 
--- location.php
+++ location.php
@@ -2,11 +2,7 @@
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
 	if (isset($_GET['search'])){
-		$result= ($_GET['search']);
-        // $result2= ($_GET['search']) + 10 ;
-        $timestamp = strtotime ($result);
-        $timestamp2 = $timestamp + 10;
-        
+		$result= ($_GET['search']);      
 	}
 }
 ?>
@@ -17,20 +13,54 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         <meta charset="UTF-8">
     </head>
     <body>
-        <h1>Lets Go </h1>
+    <style>
+    body {
+        background: #152238 }
+    section {
+        background: black;
+        color: white;
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+    </style>
+        
         <?php
-        // $position = file_get_contents('https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=1436029892,1436029902&units=miles');
-        // echo $position;
-        $position = file_get_contents('https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps='.$timestamp.',' .$timestamp2.'&units=miles');
-        $arr = json_decode($position, true);
-        $longitude = $arr[0]['longitude'];
-        $latitude = $arr[0]['latitude'];
-    
-        $location = file_get_contents('https://api.wheretheiss.at/v1/coordinates/'.$longitude.','.$latitude);  
-        $arr2 = json_decode($location, true);
-        $country = $arr2['country_code'];
-        echo $country;  
+        // echo $result;
+        $tanda='----------------------';
+        $timestart = strtotime ($result.'-70 minutes');
+        $timeend = strtotime ($result.'+50 minutes');
+        $input= $timestart;
+
+        while ($timestart <= $timeend){
+            $input = strtotime('+10 minutes',$timestart);
+            $timestamp = strtotime ($input);
+            $timestamp2 = $timestamp + 10;
+
+            $position = file_get_contents('https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps='.$timestamp.',' .$timestamp2.'&units=miles');
+
+            // var_dump(json_decode($position));
+            $arr = json_decode($position, true);
+            $longitude = $arr[0]['longitude'];
+            $latitude = $arr[0]['latitude'];
+
+            $location = file_get_contents('https://api.wheretheiss.at/v1/coordinates/'.$longitude.','.$latitude);  
+            $arr2 = json_decode($location, true);
+            $country = $arr2['country_code'];
+            $timestart = $input;
+        }
         ?>
+        <section>
+            <h1> ISS LOCATION  </h1>
+            <hr>
+            <br>
+            <p><?php echo date('d/m/Y H:i:s',$input); ?> (UTC +0)--- <?php echo $country; ?> </p>
+            <br>
+        </section>
+
     </body>
 
 </html>
